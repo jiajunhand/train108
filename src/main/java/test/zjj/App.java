0749366c3cbd7c062c3a6562747207c30896e6b2
unknown <1271637512@qq.com>
2016-08-12 18:25:08 +0800

    first commit

---
+++ src/main/java/test/zjj/App.java
@@ -0,0 +1,13 @@
+package test.zjj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldzjj!" );
+    }
+}
